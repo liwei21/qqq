d34b3b5e58304667cfb939e7c7537c0a0671f01e
Administrator <913168616>
2015-04-30 15:26:48 +0800

    Initial commitqqq           同步的DataUrl

---
+++ app/src/main/java/DataUrl/urlbeans.java
@@ -0,0 +1,103 @@
+package DataUrl;
+
+/**
+ * Created by Administrator on 2015/4/30.
+ */
+public class urlbeans {
+    private String url="http://api.zhongchou.cn/deal/recommendlist?offset=0";
+
+    /*主页ListView接口：
+GET http://api.zhongchou.cn/deal/recommendlist?offset=0
+
+http://api.zhongchou.cn/deal/list?offset=0&v=1
+
+
+2.listView接口加载数据
+GET http://api.zhongchou.cn/deal/recommendlist?offset=10 HTTP/1.1
+DIVERSION-VERSION: 2
+SESSION-TOKEN: _d4bef279498ac2926c3d28cce9d6049
+Host: api.zhongchou.cn
+Connection: Keep-Alive
+User-Agent: y 4.0 android19
+Cookie: PHPSESSID=pcokks66hn0ctcu5lmhr17qff7
+Cookie2: $Version=1
+3.
+GET http://api.zhongchou.cn/deal/recommendlist?offset=20 HTTP/1.1
+DIVERSION-VERSION: 2
+SESSION-TOKEN: _d4bef279498ac2926c3d28cce9d6049
+Host: api.zhongchou.cn
+Connection: Keep-Alive
+User-Agent: y 4.0 android19
+Cookie: PHPSESSID=pcokks66hn0ctcu5lmhr17qff7
+Cookie2: $Version=1
+4.
+GET http://api.zhongchou.cn/deal/recommendlist?offset=30 HTTP/1.1
+DIVERSION-VERSION: 2
+SESSION-TOKEN: _d4bef279498ac2926c3d28cce9d6049
+Host: api.zhongchou.cn
+Connection: Keep-Alive
+User-Agent: y 4.0 android19
+Cookie: PHPSESSID=pcokks66hn0ctcu5lmhr17qff7
+Cookie2: $Version=1
+
+5
+GET http://api.zhongchou.cn/deal/recommendlist?offset=40 HTTP/1.1
+DIVERSION-VERSION: 2
+SESSION-TOKEN: _d4bef279498ac2926c3d28cce9d6049
+Host: api.zhongchou.cn
+Connection: Keep-Alive
+User-Agent: y 4.0 android19
+Cookie: PHPSESSID=pcokks66hn0ctcu5lmhr17qff7
+Cookie2: $Version=1
+
+6.
+GET http://api.zhongchou.cn/deal/recommendlist?offset=50 HTTP/1.1
+DIVERSION-VERSION: 2
+SESSION-TOKEN: _d4bef279498ac2926c3d28cce9d6049
+Host: api.zhongchou.cn
+Connection: Keep-Alive
+User-Agent: y 4.0 android19
+Cookie: PHPSESSID=pcokks66hn0ctcu5lmhr17qff7
+Cookie2: $Version=1
+7
+GET http://api.zhongchou.cn/deal/recommendlist?offset=60 HTTP/1.1
+DIVERSION-VERSION: 2
+SESSION-TOKEN: _d4bef279498ac2926c3d28cce9d6049
+Host: api.zhongchou.cn
+Connection: Keep-Alive
+User-Agent: y 4.0 android19
+Cookie: PHPSESSID=pcokks66hn0ctcu5lmhr17qff7
+Cookie2: $Version=1
+------------------------------------------------------------------------------------
+
+全部：
+http://api.zhongchou.cn/deal/list?offset=0&categoryID=797484e981d914c7d7c9e72f&sort=sb &v=1
+科技
+http://api.zhongchou.cn/deal/list?offset=0&categoryID=1caaa00704ea0af53adad706&sort=sb&v=1
+公益
+http://api.zhongchou.cn/deal/list?offset=0&categoryID=fb9650095e7657e96c79de2f&sort=sb&v=1
+出版
+http://api.zhongchou.cn/deal/list?offset=0&categoryID=1caaa00704cd0af53adad72f&sort=sb
+娱乐：
+http://api.zhongchou.cn/deal/list?offset=0&categoryID=cd5627cc1bd23adaf6f7d406&sort=sb
+艺术：
+http://api.zhongchou.cn/deal/list?offset=0&categoryID=653b96c77508497657e9d12f&sort=sb
+农业：
+http://api.zhongchou.cn/deal/list?offset=0&categoryID=48b2c9e842298511940adb2f&sort=sb
+商铺
+http://api.zhongchou.cn/deal/list?offset=0&categoryID=54a509dbe5d03b6ba5c6d206&sort=sb
+其他
+http://api.zhongchou.cn/deal/list?offset=0&categoryID=4a9d39f428daf6f73b6bd52f&sort=sb
+
+原始会
+没抓到
+
+众筹筑屋
+http://api.zhongchou.cn/deal/list?offset=0&categoryID=653b96c7752f497657e9d106&sort=sb
+
+【最新动态】：
+感受是个美丽
+http://api.zhongchou.cn/deal/getdetail?projectID=6c606d294913cde18b24ae94
+下面listView:
+http://api.zhongchou.cn/deal/getallitems?projectID=6c606d294913cde18b24ae94*/
+}
