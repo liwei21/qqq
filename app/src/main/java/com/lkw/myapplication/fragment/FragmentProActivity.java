2ff46f313637d32c05f20450b42974f4144b07f2
LKW <771915131@qq.com>
2015-05-02 17:00:24 +0800

    工具12

---
+++ app/src/main/java/com/lkw/myapplication/fragment/FragmentProActivity.java
@@ -0,0 +1,9 @@
+package com.lkw.myapplication.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by LKW on 2015/5/2.
+ */
+public class FragmentProActivity extends Fragment {
+}
