d9947c21e9e0bdcd08964aba24e8816435515d75
Administrator <913168616>
2015-05-05 19:20:02 +0800

    Spinner数据

---
+++ app/src/main/java/com/lkw/myapplication/bean/Spinnerdata.java
@@ -0,0 +1,10 @@
+package com.lkw.myapplication.bean;
+
+/**
+ * Created by Administrator on 2015/5/5.
+ */
+public class Spinnerdata {
+    public static final String[] quanbu = {"全部", "科技", "公益", "出版", "娱乐", "艺术", "农业", "商铺", "其他", "众筹筑屋"};
+    public static final String order[]={"默认排序","最多支持","最多关注","最近更新"};
+    public static final String state[]={"全部状态","筹款中","已成功"};
+}
