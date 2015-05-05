a82d0ba4e4c36ef32887d5cca486949ccbc4ad06
LKW <771915131@qq.com>
2015-05-05 10:15:14 +0800

    更新的改变

--- app/src/main/java/com/lkw/myapplication/UsercenterActivity.java
+++ app/src/main/java/com/lkw/myapplication/UsercenterActivity.java
@@ -4,16 +4,30 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 
-public class UsercenterActivity extends ActionBarActivity {
+public class UsercenterActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private RelativeLayout browding_goback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usercenter);
+
+        browding_goback = (RelativeLayout) findViewById(R.id.browding_goback);
+        browding_goback.setOnClickListener(this);
     }
 
 
-
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.browding_goback:
+                finish();
+                break;
+        }
+    }
 }
